428f06a0854c1e252be4a7c6e46ca6b35fd812ae
Antoine Trémouroux <antoine.trx@hotmail.com>
2024-02-15 19:49:22 +0100

    Add project files.

---
+++ NetworkListener/Receiver.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace NetworkReceiver
+{
+    class Receiver
+    {
+        static UdpClient udpClient;
+        static IPEndPoint remoteEndPoint;
+        static FileStream fileStream;
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: Receiver <port> <outputFile>");
+                return;
+            }
+
+            int port = int.Parse(args[0]);
+            string outputFile = args[1];
+
+            udpClient = new UdpClient(port);
+            remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            fileStream = File.OpenWrite(outputFile);
+
+            while (true)
+            {
+                byte[] data = udpClient.Receive(ref remoteEndPoint);
+                ProcessReceivedData(data);
+
+                // Implement connection termination logic based on received flags (RST, FIN)
+                // ...
+
+                // Send ACK packet with the sequence number of the last received packet
+                SendAckPacket(GetLastSequenceNumber(data));
+            }
+        }
+
+        static void ProcessReceivedData(byte[] data)
+        {
+            // Implement logic to process received data and write to the file stream
+            // For simplicity, just write the received data to the file
+            fileStream.Write(data, 0, data.Length);
+        }
+
+        static void SendAckPacket(ushort lastSequenceNumber)
+        {
+            byte[] ackData = BitConverter.GetBytes(lastSequenceNumber);
+            byte[] ackPacket = ConstructPacket(0, 0, 0, 0, ackData);
+
+            udpClient.Send(ackPacket, ackPacket.Length, remoteEndPoint);
+        }
+
+        static ushort GetLastSequenceNumber(byte[] data)
+        {
+            //For now, seq numb juste be last to
+            return BitConverter.ToUInt16(data, data.Length - 2);
+        }
+
+        static byte[] ConstructPacket(ushort sequenceNumber, byte synFlag, byte ackFlag, byte finFlag, byte[] data)
+        {
+            // Implement logic to construct a packet with given parameters
+            byte[] packet = new byte[2 + 2 + 1 + 1 + 1 + data.Length];
+
+            // Copy sequence number
+            BitConverter.GetBytes(sequenceNumber).CopyTo(packet, 0);
+
+            // Set flags
+            packet[2] = synFlag;
+            packet[3] = ackFlag;
+            packet[4] = finFlag;
+
+            // Copy data
+            data.CopyTo(packet, 5);
+
+            return packet;
+        }
+    }
+}
---
+++ NetworkSenderµ/Sender.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace NetworkSender
+{
+
+    class Sender
+    {
+        static UdpClient udpClient;
+        static IPEndPoint remoteEndPoint;
+        static FileStream fileStream;
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Usage: Sender <receiverIp> <receiverPort> <inputFile>");
+                return;
+            }
+
+            string receiverIp = args[0];
+            int receiverPort = int.Parse(args[1]);
+            string inputFile = args[2];
+
+            udpClient = new UdpClient();
+            remoteEndPoint = new IPEndPoint(IPAddress.Parse(receiverIp), receiverPort);
+            fileStream = File.OpenRead(inputFile);
+
+            // Implement connection initiation logic (SYN) and data transmission
+            SendSynPacket();
+            
+            //TODO add behavior for sending packet
+            //- check if all packet come to right receiver
+            //- manage sequence number
+            //- manage re send of packet if loss
+
+            // Implement connection termination logic (FIN, RST)
+            SendFinPacket();
+        }
+
+        static void SendSynPacket()
+        {
+            byte[] synPacket = ConstructPacket(0, 1, 0, 0, new byte[0]);
+
+            udpClient.Send(synPacket, synPacket.Length, remoteEndPoint);
+
+            // Wait for SYN-ACK
+            byte[] synAckData = udpClient.Receive(ref remoteEndPoint);
+            ushort synAckSequenceNumber = GetLastSequenceNumber(synAckData);
+
+            // Respond with final SYN
+            byte[] finalSynPacket = ConstructPacket(synAckSequenceNumber, 1, 0, 0, new byte[0]);
+            udpClient.Send(finalSynPacket, finalSynPacket.Length, remoteEndPoint);
+        }
+
+        static void SendDataPacket(byte[] data, ushort sequenceNumber)
+        {
+            byte[] dataPacket = ConstructPacket(sequenceNumber, 0, 0, 0, data);
+            udpClient.Send(dataPacket, dataPacket.Length, remoteEndPoint);
+        }
+
+        static void SendFinPacket()
+        {
+            //TODO
+        }
+
+        static void SendRstPacket()
+        {
+            //TODO
+        }
+
+        static ushort GetLastSequenceNumber(byte[] data)
+        {
+            // last two bytes is seq numb (see if ok ) 
+            return BitConverter.ToUInt16(data, data.Length - 2);
+        }
+
+        static byte[] ConstructPacket(ushort sequenceNumber, byte synFlag, byte ackFlag, byte finFlag, byte[] data)
+        {
+            // Implement logic to construct a packet with given parameters
+            byte[] packet = new byte[ 5 + data.Length];
+
+            // Copy sequence number
+            BitConverter.GetBytes(sequenceNumber).CopyTo(packet, 0);
+
+            // Set flags
+            packet[2] = synFlag;
+            packet[3] = ackFlag;
+            packet[4] = finFlag;
+
+            // Copy data
+            data.CopyTo(packet, 5);
+
+            return packet;
+        }
+    }
+
+}
